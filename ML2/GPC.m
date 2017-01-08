d22ff1780c6e0b1e3308619b10594de76511277f
0.5a <nima.gbani@gamil.com>
2017-01-08 23:56:34 +0100

    showing accuracy

--- ML2/GPC.m
+++ ML2/GPC.m
@@ -1,10 +1,12 @@
 % Goal: Gaussian Process Classification - GPC
+set(0,'DefaultFigureWindowStyle','docked')
+
 clc; clear; close all
-rng('default');
-maxiter = 1000;
+%rng('default');
 
 %% setup test Data points
-n1 = 100; n2 = 50;
+n1 = 100; n2 = 100;
+n_test = 50;
 
 S1 = eye(2); S2 = [1,0.95;0.95,1];
 m1 = [0.75, 0]; m2 = [-0.75, 0];
@@ -12,14 +14,16 @@ m1 = [0.75, 0]; m2 = [-0.75, 0];
 x1 = mvnrnd(m1,S1,n1);
 x2 = mvnrnd(m2,S2,n2);
 
-x = [x1;x2]; y = [-ones(n1,1); ones(n2,1)];
-x_test = x;
-plot(x1(:,1), x1(:,2), 'b+'); hold on;
-plot(x2(:,1), x2(:,2), 'r+');
+x = [x1(1:n1-n_test,:);x2(1:n2-n_test,:)]; y = [-ones(n1-n_test,1); ones(n2-n_test,1)];
+x_test = [x1(n_test+1:end,:);x2(n_test+1:end,:)]; y_test = [-ones(n_test,1); ones(n_test,1)];
+
+plot(x(y==1,1), x(y==1,2), 'b+', x(y==-1,1), x(y==-1,2), 'r+'); hold on;
+plot(x_test(y_test==1,1), x_test(y_test==1,2), 'b*', x_test(y_test==-1,1), x_test(y_test==-1,2), 'r*'); hold on;
 axis([-4 4 -4 4]);
 
 %% predictive gaussian parameter finding (probit likelihood)
 % based on algo 3.1 page 65 GPML book
+maxiter = 1000;
 
 sigmaf = 1; l = 1; sigman = 1;
 K = get_kernel(x,x,sigmaf,l,sigman);
@@ -55,10 +59,9 @@ for iter = 1:maxiter
         old_log_marg = new_log_marg;
     end
 end  
-
-%% Binary classification with  approximated Laplace GPC
-% algo 3.2 GPML
 fhat = f;
+
+%% optimizing kernel hyperparameters
 
 p = [1, 0.1];
 
@@ -71,6 +74,9 @@ fun = @(p) -(-0.5*fhat'*(get_kernel(x,x,p(1),p(2),0)\fhat) ...
 p = fminsearch(fun,p);
 
 sigmaf=p(1); l=p(2);
+
+%% Binary classification with  approximated Laplace GPC
+% algo 3.2 GPML
 
 npf = normpdf(fhat); cpyf = normcdf(y.*fhat);
     
@@ -93,9 +99,23 @@ v = L\(sW.*K_s);
 y_test_var = diag(K_ss - v'*v);% uncertainty
 y_test_mean = normcdf(real(fs./sqrt(1+y_test_var)));% best prediction
 
-plot(x(y_test_mean>=0.5,1), x(y_test_mean>=0.5,2), 'rO'); hold on;
-plot(x(y_test_mean<0.5,1), x(y_test_mean<0.5,2), 'bO'); hold on;
-title('Gaussian Process Classification')
+%plot(x(y_test_mean>=0.5,1), x(y_test_mean>=0.5,2), 'rO'); hold on;
+%plot(x(y_test_mean<0.5,1), x(y_test_mean<0.5,2), 'bO'); hold on;
+
+c1_thresh = 0.5; c2_thresh = 1 - c1_thresh;
+y_test_temp = y_test_mean;
+
+y_test_temp(y_test_mean>=c1_thresh) = 1;
+y_test_temp(y_test_mean<c2_thresh) = -1;
+
+acc = sum(y_test==y_test_temp);
+
+plot(x(y==1,1), x(y==1,2), 'b+', x(y==-1,1), x(y==-1,2), 'r+'); hold on;
+plot(x_test(y_test==1,1), x_test(y_test==1,2), 'b*', x_test(y_test==-1,1), x_test(y_test==-1,2), 'r*');
+ 
+plot(x_test(y_test_mean>=c1_thresh,1), x_test(y_test_mean>=c1_thresh,2), 'bO',...
+    x_test(y_test_mean<c2_thresh,1), x_test(y_test_mean<c2_thresh,2), 'rO');
+axis([-4 4 -4 4]);title(sprintf('Gaussian Process Classification\nAccuracy = %2.2f%%',acc/100))
 
 %% computation of the covariance function
 function K = get_kernel(X1,X2,sigmaf,l,sigman)
